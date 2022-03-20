230b9a63cf5c833aeffa06a4c07af864d85cc166
jobinben <1021137079@qq.com>
2022-03-20 19:59:39 +0800

    practice crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
+import { UsersController } from './modules/users/users.controller';
+import { DetailModule } from './modules/detail/detail.module';
+import { UsersService } from './modules/users/users.service';
 
 @Module({
-  imports: [],
+  imports: [DetailModule],
   controllers: [AppController, UsersController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
